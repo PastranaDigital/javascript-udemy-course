05584ca26466277cb804c31ec68de95178d3a2ff
Omar Pastrana <opastrana@salesforce.com>
2021-05-21 10:44:27 -0500

    through video 16

--- Fundamentals-Part-1/assignments.js
+++ Fundamentals-Part-1/assignments.js
@@ -1,6 +1,6 @@
 let country = "United States";
 let continent = "North America";
-let population = 325000000;
+let population = 325;
 
 // console.log("country: " + country);
 // console.log("continent: " + continent);
@@ -8,9 +8,19 @@ let population = 325000000;
 
 const isIsland = false;
 const language = "English";
+const finlandPopulation = 6;
+const description1 = "Portugal is in Europe, and its 11 million people speak portuguese";
 
-console.log(isIsland);
-console.log("population: " + population);
-console.log("country: " + country);
-console.log(language);
-console.log(typeof isIsland);
+const description2 = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+
+// console.log(description2);
+
+// console.log(isIsland);
+// population += 1;
+// console.log("population: " + population);
+// console.log("country: " + country);
+// console.log(language);
+// console.log(typeof isIsland);
+
+// console.log("1/2 population: " + population / 2);
+// console.log(population > finlandPopulation);
--- Fundamentals-Part-1/script.js
+++ Fundamentals-Part-1/script.js
@@ -1,6 +1,12 @@
 let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 let firstName = "Omar";
 let lastName = "Pastrana";
-console.log(firstName + " " + lastName);
+// console.log(firstName + " " + lastName);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+// console.log(ageJonas, ageSarah);
