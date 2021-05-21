cc4ad3de180fa8f52fe544ae9b0b3033eeeaeabd
Omar Pastrana <opastrana@salesforce.com>
2021-05-21 09:46:47 -0500

    through video 13

---
+++ Fundamentals-Part-1/assignments.js
@@ -0,0 +1,16 @@
+let country = "United States";
+let continent = "North America";
+let population = 325000000;
+
+// console.log("country: " + country);
+// console.log("continent: " + continent);
+// console.log("population: " + population);
+
+const isIsland = false;
+const language = "English";
+
+console.log(isIsland);
+console.log("population: " + population);
+console.log("country: " + country);
+console.log(language);
+console.log(typeof isIsland);
---
+++ Fundamentals-Part-1/script.js
@@ -0,0 +1,6 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Omar";
+let lastName = "Pastrana";
+console.log(firstName + " " + lastName);
