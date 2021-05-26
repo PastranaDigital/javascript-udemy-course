82c51d6dbadfef2c872fa4a86c9c956842c9e539
Omar Pastrana <omar.pastrana@gmail.com>
2021-05-26 06:20:05 -0500

    through video 60

--- Developer-Skills/script.js
+++ Developer-Skills/script.js
@@ -1,9 +1,47 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
-const temperatures = [5, 3, 2, 1];
-let temperaturesSorted = [];
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const findAmplitude = function (array) {
+	let max;
+	let min;
+	for (let i = 0; i < array.length; i++) {
+		if (typeof array[i] === "number") {
+			if (i === 0) {
+				max = min = array[i];
+			} else if (array[i] >= max) {
+				max = array[i];
+			} else if (array[i] <= min) {
+				min = array[i];
+			}
+		}
+	}
+	return max - min;
+};
+console.log(findAmplitude(temperatures));
+
+const findAmplitude2 = function (array1, array2) {
+	let max;
+	let min;
+	let newArray = array1.concat(array2);
+	for (let i = 0; i < newArray.length; i++) {
+		if (typeof newArray[i] === "number") {
+			if (i === 0) {
+				max = min = newArray[i];
+			} else if (newArray[i] >= max) {
+				max = newArray[i];
+			} else if (newArray[i] <= min) {
+				min = newArray[i];
+			}
+		}
+	}
+	return max - min;
+};
+console.log(findAmplitude2(temperatures, [14, -8, 0]));
+
+// const temperatures = [5, 3, 2, 1];
+// let temperaturesSorted = [];
 
 // temperatures.splice(2, 0, 99);
 
@@ -38,25 +76,22 @@ let temperaturesSorted = [];
 // 	return tempArray;
 // };
 
-const sortArray = function (array) {
-	const tempArray = array;
-	for (let i = 0; i < array.length; i++) {
-		for (let j = 1; j < array.length; j++) {
-			if (array[i] > array[j]) {
-				break;
-			} else if (array[i] < array[j]) {
-                tempArray[i] = array[j];
-                tempArray[j] = array[i];
-                break;
-            }
-		}
-	}
-	return tempArray;
-};
+// const sortArray2 = function (array) {
+// 	const tempArray = array;
+// 	for (let i = 0; i < array.length; i++) {
+// 		for (let j = 1; j < array.length; j++) {
+// 			if (array[i] > array[j]) {
+// 				break;
+// 			} else if (array[i] < array[j]) {
+// 				tempArray[i] = array[j];
+// 				tempArray[i + 1] = array[i];
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	return tempArray;
+// };
 
-temperaturesSorted = sortArray(temperatures);
-// console.log(temperatures);
-console.log(temperaturesSorted);
-
-// temperaturesSorted = sortArray(temperaturesSorted);
+// temperaturesSorted = sortArray2(temperatures);
+// // console.log(temperatures);
 // console.log(temperaturesSorted);
