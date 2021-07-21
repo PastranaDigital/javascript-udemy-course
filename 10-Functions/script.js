053cb2d840ca1552b471b6df2407cdb88c0c5bb8
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-07-21 17:19:28 -0500

    through video 132

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 //! Default Parameters
 // const bookings = [];
 
 // const createBooking = function(
-//     flightNum, 
-//     numPassengers = 1, 
+//     flightNum,
+//     numPassengers = 1,
 //     price = 199 * numPassengers
 //     ) {
 //     //? old way of defaulting parameters
 //     // numPassengers = numPassengers || 1;
 //     // price = price || 199;
-    
+
 //     const booking = {
 //         flightNum,
 //         numPassengers,
@@ -64,4 +64,93 @@
 //? High Order Functions: a function that receives another function as an argument, that returns a new function of both
 //? this is only possible because of first-class functions
 
-//! Functions Accepting Callback Functions
+// //! Functions Accepting Callback Functions
+// const oneWord = function (str) {
+// 	return str.replace(/ /g, "").toLowerCase(); // "/ /g" is regex for any spaces
+// };
+
+// const upperFirstWord = function (str) {
+// 	const [first, ...others] = str.split(" ");
+// 	console.log([first.toUpperCase(), ...others].join(" "));
+// 	return [first.toUpperCase(), ...others].join(" ");
+// };
+
+// //? Higher Order function (like addEventListener)
+// //? this allows "abstraction" - removing the details from one function to another "lower level" function
+// const transformer = function (str, fn) {
+// 	console.log(`Original string: ${str}`);
+// 	console.log(`Transformed string: ${fn(str)}`);
+
+// 	console.log(`Transformed by: ${fn.name} function`);
+// 	console.log("");
+// };
+
+// transformer("JavaScript is the best!", upperFirstWord);
+// transformer("JavaScript is the best!", oneWord);
+
+// //? example example (addEventListener)
+// const high5 = function () {
+// 	console.log("5");
+// };
+// document.body.addEventListener("click", high5);
+
+// //? example callback (forEach)
+// ["Alpha", "Bravo", "Charlie"].forEach(upperFirstWord);
+
+// //! Functions returning Functions
+// //? used in functional programming
+// const greet = function (greeting) {
+// 	return function (name) {
+// 		console.log(`${greeting} ${name}`);
+// 	};
+// };
+
+// const greeterHey = greet("Hey"); // This is now a function with the greeting pre-loaded
+// greeterHey("Omar");
+// greeterHey("Maya");
+
+// greet("Hello")("Jonas");
+
+// // writing only as arrow functions
+// const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+// greetArrow("Wassup")("Emily");
+
+//! The call and apply method
+
+const lufthansa = {
+	airline: "Lufthansa",
+	iataCode: "LH",
+	bookings: [],
+	// book: funtion() {}
+	book(flightNum, name) {
+		// this is calling to the object from which the function was called
+		console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+		this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+	},
+};
+
+lufthansa.book(239, "Omar Pastrana");
+lufthansa.book(635, "Emily Pastrana");
+console.log(lufthansa);
+
+const eurowings = {
+	airline: "Eurowings",
+	iataCode: "EW",
+	bookings: [],
+	//? adding the function again here would be against DRY method of programming... so abstract it
+};
+
+const book = lufthansa.book; // copies the function
+// book(230, 'Eva Pastrana'); // will not work because this is undefined
+
+//? call method
+book.call(eurowings, 230, "Eva Pastrana"); // first argument defines the 'this' keyword
+book.call(lufthansa, 635, "Eva Pastrana"); // first argument defines the 'this' keyword
+
+//? apply method
+const flightData = [428, "Gabriel Pastrana"];
+// book.apply(eurowings, flightData); // old way of doing this since we have the spread operator now
+
+book.call(eurowings, ...flightData);
+console.log(eurowings);
