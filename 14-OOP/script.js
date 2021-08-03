1aea6ab079611e9b6ddc51827e0a3ae031a1fa0e
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-08-03 06:43:39 -0500

    through video 224

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -398,3 +398,200 @@
 // jay.init("Jay", 2001, "Math");
 // jay.introduce();
 // jay.calcAge();
+
+//! Another Class Example
+class Account {
+	//* Public fields (they are added on the instances, not the prototype)
+	locale = navigator.language;
+
+	//* Private fields (instances only)
+	#movements = [];
+	#pin; // create the variable out here then set it inside
+
+	constructor(owner, currency, pin) {
+		this.owner = owner;
+		this.currency = currency;
+		//? setting values without needing input
+		//! protected by using "_"
+		this.#pin = pin;
+		// this._movements = [];
+		// this.locale = navigator.language;
+
+		console.log(`Thanks for opening an account ${owner}`);
+	}
+
+	//* Public methods
+	getMovements() {
+		return this.#movements;
+	}
+
+	//? public interface to our object (API)
+	deposit(val) {
+		this.#movements.push(val);
+		return this;
+	}
+
+	withdraw(val) {
+		this.deposit(-val);
+		return this;
+	}
+
+	// //? should be private in object and not accessible outside
+	// _approveLoan(val) {
+	// 	return true;
+	// }
+
+	requestLoan(val) {
+		if (this.#approveLoan(val)) {
+			this.deposit(val);
+			console.log(`Loan approved`);
+			return this;
+		}
+	}
+
+	//! static methods are only available on the class itself, not the instances
+	static helper() {
+		console.log("Helper");
+	}
+
+	//* Private methods
+	//? should be private in object and not accessible outside
+	#approveLoan(val) {
+		return true;
+	}
+}
+
+const acc1 = new Account("Omar", "USD", 1111);
+console.log(acc1);
+//? instead of interacting directly with properties like this. We should instead make methods that will interact with the properties
+// acc1.movements.push(250);
+// acc1.movements.push(-125);
+// console.log(acc1);
+acc1.deposit(250);
+acc1.withdraw(125);
+acc1.requestLoan(1250);
+// acc1.approveLoan(1250); //? should NOT be accessible externally
+
+console.log(acc1);
+// console.log(acc1.pin); //? should NOT be accessible externally
+
+//! Encapsulation: Protected Properties and Methods
+//? to keep some methods private
+//? will help prevent code interface (ex: movements.push)
+
+//? by using the "_" it tells the developers that this method or property is protected and they should not write code to access it directly
+console.log(acc1.getMovements()); // used to access the protected properties
+
+//! Encapsulation: Private Class Fields and Methods
+//? NOT ready yet, still in beta
+//* 4 groups
+//* Public fields
+console.log(acc1.locale);
+//* Private fields
+// console.log(acc1.#movements); // WONT WORK because it is private
+//* Public methods
+//* Private methods
+// console.log(acc1.#approveLoan); // WONT WORK because it is private & still in beta so it is read as a field by the Chrome browser
+
+//? using the static method "helper"
+Account.helper();
+// acc1.helper(); // wont work since it is an instance
+
+//! Chaining Methods Together
+// acc1.deposit(99).withdraw(33); //NOT GOING TO WORK!
+//? acc1.deposit returns nothing so the next call is working with undefined
+//? we fix it by adding "return this" at the end of the method
+acc1.deposit(99).withdraw(33).deposit(100).requestLoan(300).withdraw(500);
+console.log(acc1.getMovements());
+
+//! ES6 Classes Summary
+//? child class extends parent (inheriting methods and fields)
+//? public fields are available on each instance
+//? private fields are not available ouside of class
+//? static public fields are only available inside the class
+//? call to parent (super) class (necessary with extend) Needs to happen before accessing "this"
+//? constructor needed in new operator, mandatory in regular class, might be omitted in a child class
+//? instance property (available on created object)
+//? redefining a private field
+//? public method
+//? referencing private field and method
+//? private method (might not yet work in your browser so you can "Fake" by using _ instead of #)
+//? Getter method
+//? Setter method (use _ to set property with same name as method, and also add getter)
+//? Static method (available only in class. Can not access instance properties nor methods, only static ones)
+//? static methods are used as helper methods in the class
+//? Creating a new object with new operator
+
+//* Classes are just "syntactic sugar" over constructor functions
+//* Classes are not hoisted
+//* Classes are first-class citizens
+//* Class body is always executed in strict mode
+
+class Person {
+	constructor(fullName, birthYear) {
+		this.fullName = fullName;
+		this.birthYear = birthYear;
+	}
+
+	//* Instance Methods
+	calcAge() {
+		console.log(2021 - this.birthYear);
+	}
+
+	introduce() {
+		console.log(`Hello ${this.fullName}!`);
+	}
+
+	get age() {
+		return 2021 - this.birthYear;
+	}
+
+	//? setter for a property name that already exists
+	set fullName(name) {
+		console.log(name);
+		if (name.includes(" ")) this._fullName = name;
+		// used _fullName since fullName is already a property
+		else alert(`${name} is not a full name!`);
+	}
+	//? since our setter was referencing the same name as a property we have to use a getter for when we want (ex: jessica.fullName)
+	get fullName() {
+		return this._fullName;
+	}
+}
+
+class Athlete extends Person {
+	competition = "Crossfit Games";
+	#athleteNumber = "000"; // don't want to be able to change outside of class
+	static numEvents = 10;
+
+	constructor(fullName, birthYear, location, group, level) {
+		super(fullName, birthYear);
+		this.location = location;
+		this.group = group;
+		this.level = level;
+		this.#athleteNumber = String(Math.floor(Math.random() * 1000 + 1)); //.padStart("0", 3);
+	}
+
+	introduce() {
+		console.log(
+			`Welcome, ${this.group}-${this.level} athlete ${this.fullName} #${this.#athleteNumber} from ${
+				this.location
+			}!`
+		);
+	}
+
+	get athleteNumber() {
+		return this.#athleteNumber;
+	}
+
+	static printEvents() {
+		console.log(`You will be competing in ${this.numEvents} events this year.`);
+	}
+}
+
+const jacob = new Athlete("Jacob Heppner", 1994, "United States", "Male", "RX");
+console.log(jacob.athleteNumber);
+// console.log(jacob.#athleteNumber);
+jacob.introduce();
+// jacob.printEvents();
+Athlete.printEvents();
