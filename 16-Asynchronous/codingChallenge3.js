2bdc2e2ed28d11b5401b4ce4ba59e52a434ddace
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-09-30 16:57:17 -0500

    through video 263

--- 16-Asynchronous/codingChallenge3.js
+++ 16-Asynchronous/codingChallenge3.js
@@ -29,36 +29,36 @@ const createImage = function (imgPath) {
 	});
 };
 
-//? Part 1
-const loadNPause = async function (img1, img2) {
-	let currentImg;
-	try {
-		const data = await Promise.all([createImage(img1), wait(2), createImage(img2), wait(2)]);
-		console.log(data);
-		data.map((d) => (currentImg = d[0]));
-		// currentImg = data[0];
-		// currentImg.style.display = "none";
-	} catch (err) {
-		console.error(err.message);
-	}
-};
+// //? Part 1
+// const loadNPause = async function (img1, img2) {
+// 	let currentImg;
+// 	try {
+// 		const data = await Promise.all([createImage(img1), wait(2), createImage(img2), wait(2)]);
+// 		console.log(data);
+// 		data.map((d) => (currentImg = d[0]));
+// 		// currentImg = data[0];
+// 		// currentImg.style.display = "none";
+// 	} catch (err) {
+// 		console.error(err.message);
+// 	}
+// };
 
-loadNPause("img/img-1.jpg", "img/img-2.jpg");
+// loadNPause("img/img-1.jpg", "img/img-2.jpg");
 
-//? Part 2
-const loadAll = async function (imgArr) {
-	try {
-		imgArr.map;
-	} catch (error) {
-		console.error(error.message);
-	}
-};
+// //? Part 2
+// const loadAll = async function (imgArr) {
+// 	try {
+// 		imgArr.map;
+// 	} catch (error) {
+// 		console.error(error.message);
+// 	}
+// };
 
 // loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img3.jpg']);
 
-//! solution
+//! Part 1 solution
 
-// let currentImg;
+let currentImg;
 
 // createImage("img/img-1.jpg")
 // 	.then((img) => {
@@ -79,3 +79,41 @@ const loadAll = async function (imgArr) {
 // 		currentImg.style.display = "none";
 // 	})
 // 	.catch((err) => console.error(err));
+
+const loadNPause = async function () {
+	try {
+		// load image 1
+		let img = await createImage("img/img-1.jpg");
+		console.log("Image 1 loaded");
+		await wait(2);
+		img.style.display = "none";
+
+		// load image 2
+		img = await createImage("img/img-2.jpg");
+		console.log("Image 2 loaded");
+		await wait(2);
+		img.style.display = "none";
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+// loadNPause();
+
+//! Part 2 solution
+
+const loadAll = async function (imgArray) {
+	try {
+		const imgs = imgArray.map(async (img) => await createImage(img));
+		console.log(imgs); // an array of promises
+		const imgsEl = await Promise.all(imgs);
+		console.log(imgsEl); // separates them
+		imgsEl.forEach((img) => {
+			img.classList.add("parallel");
+		});
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+loadAll(["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"]);
